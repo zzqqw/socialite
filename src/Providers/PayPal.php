2fe592aadf1acd96e54a71ec18e1b04abc2c2898
overtrue <anzhengchao@gmail.com>
2024-06-25 08:28:18 +0800

    refactor: 移除不必要的依赖

--- src/Providers/PayPal.php
+++ src/Providers/PayPal.php
@@ -8,8 +8,9 @@ use Overtrue\Socialite\User;
 
 /**
  * Class PayPal
+ *
  * @author zhiqiang
- * @package Overtrue\Socialite\Providers
+ *
  * @see https://developer.paypal.com/docs/log-in-with-paypal/
  */
 class PayPal extends Base
@@ -17,19 +18,23 @@ class PayPal extends Base
     public const NAME = 'paypal';
 
     protected string $scopeSeparator = ' ';
+
     /**
      * @var string|null
-     * code or id_token
+     *                  code or id_token
      */
     protected ?string $responseType = Contracts\RFC6749_ABNF_CODE;
-    protected string  $flowEntry    = 'static';
 
-    protected string $authUrl     = 'https://www.paypal.com/signin/authorize';
-    protected string $tokenURL    = "https://api.sandbox.paypal.com/v1/oauth2/token";
-    protected string $userinfoURL = "https://api.paypal.com/v1/identity/openidconnect/userinfo";
+    protected string $flowEntry = 'static';
+
+    protected string $authUrl = 'https://www.paypal.com/signin/authorize';
+
+    protected string $tokenURL = 'https://api.sandbox.paypal.com/v1/oauth2/token';
+
+    protected string $userinfoURL = 'https://api.paypal.com/v1/identity/openidconnect/userinfo';
 
     protected array $scopes = [
-        'openid', 'profile', 'email', 'address'
+        'openid', 'profile', 'email', 'address',
     ];
 
     protected bool $sandbox = true;
@@ -37,22 +42,23 @@ class PayPal extends Base
     public function __construct(array $config)
     {
         parent::__construct($config);
-        $this->sandbox = (bool)$this->config->get('sandbox', false);
+        $this->sandbox = (bool) $this->config->get('sandbox', false);
         if ($this->sandbox) {
-            $this->authUrl     = 'https://www.sandbox.paypal.com/signin/authorize';
-            $this->tokenURL    = 'https://api-m.sandbox.paypal.com/v1/oauth2/token';
+            $this->authUrl = 'https://www.sandbox.paypal.com/signin/authorize';
+            $this->tokenURL = 'https://api-m.sandbox.paypal.com/v1/oauth2/token';
             $this->userinfoURL = 'https://api-m.sandbox.paypal.com/v1/identity/openidconnect/userinfo';
         }
     }
 
     /**
-     * @param string|null $responseType
      * @return $this
+     *
      * @see https://developer.paypal.com/docs/log-in-with-paypal/integrate/generate-button/
      */
     public function withResponseType(?string $responseType)
     {
         $this->responseType = $responseType;
+
         return $this;
     }
 
@@ -65,11 +71,11 @@ class PayPal extends Base
     {
         $fields = \array_merge(
             [
-                'flowEntry'                          => $this->flowEntry,
-                Contracts\RFC6749_ABNF_CLIENT_ID     => $this->getClientId(),
+                'flowEntry' => $this->flowEntry,
+                Contracts\RFC6749_ABNF_CLIENT_ID => $this->getClientId(),
                 Contracts\RFC6749_ABNF_RESPONSE_TYPE => $this->responseType,
-                Contracts\RFC6749_ABNF_SCOPE         => $this->formatScopes($this->scopes, $this->scopeSeparator),
-                Contracts\RFC6749_ABNF_REDIRECT_URI  => $this->redirectUrl,
+                Contracts\RFC6749_ABNF_SCOPE => $this->formatScopes($this->scopes, $this->scopeSeparator),
+                Contracts\RFC6749_ABNF_REDIRECT_URI => $this->redirectUrl,
             ],
             $this->parameters
         );
@@ -77,9 +83,9 @@ class PayPal extends Base
         if ($this->state) {
             $fields[Contracts\RFC6749_ABNF_STATE] = $this->state;
         }
+
         return $fields;
     }
-
 
     protected function getTokenUrl(): string
     {
@@ -87,10 +93,9 @@ class PayPal extends Base
     }
 
     /**
-     * @param string $code
-     * @return array
      * @throws \GuzzleHttp\Exception\GuzzleException
      * @throws \Overtrue\Socialite\Exceptions\AuthorizeFailedException
+     *
      * @see https://developer.paypal.com/docs/log-in-with-paypal/integrate/#link-getaccesstoken
      */
     public function tokenFromCode(string $code): array
@@ -100,46 +105,46 @@ class PayPal extends Base
             [
                 'form_params' => [
                     Contracts\RFC6749_ABNF_GRANT_TYPE => Contracts\RFC6749_ABNF_AUTHORATION_CODE,
-                    Contracts\RFC6749_ABNF_CODE       => $code,
+                    Contracts\RFC6749_ABNF_CODE => $code,
                 ],
-                'headers'     => [
-                    'Accept'        => 'application/json',
-                    'Authorization' => 'Basic ' . \base64_encode(\sprintf('%s:%s', $this->getClientId(), $this->getClientSecret())),
+                'headers' => [
+                    'Accept' => 'application/json',
+                    'Authorization' => 'Basic '.\base64_encode(\sprintf('%s:%s', $this->getClientId(), $this->getClientSecret())),
                 ],
             ]
         );
-        return $this->normalizeAccessTokenResponse((string)$response->getBody());
+
+        return $this->normalizeAccessTokenResponse((string) $response->getBody());
     }
 
     /**
-     * @param string $refreshToken
-     * @return mixed
      * @throws \GuzzleHttp\Exception\GuzzleException
      * @throws \Overtrue\Socialite\Exceptions\AuthorizeFailedException
+     *
      * @see https://developer.paypal.com/docs/log-in-with-paypal/integrate/#link-exchangerefreshtokenforaccesstoken
      */
     public function refreshToken(string $refreshToken): mixed
     {
-        $response    = $this->getHttpClient()->post(
+        $response = $this->getHttpClient()->post(
             $this->getTokenUrl(),
             [
                 'form_params' => [
-                    Contracts\RFC6749_ABNF_GRANT_TYPE    => Contracts\RFC6749_ABNF_REFRESH_TOKEN,
+                    Contracts\RFC6749_ABNF_GRANT_TYPE => Contracts\RFC6749_ABNF_REFRESH_TOKEN,
                     Contracts\RFC6749_ABNF_REFRESH_TOKEN => $refreshToken,
                 ],
-                'headers'     => [
-                    'Accept'        => 'application/json',
-                    'Authorization' => 'Basic ' . \base64_encode(\sprintf('%s:%s', $this->getClientId(), $this->getClientSecret())),
+                'headers' => [
+                    'Accept' => 'application/json',
+                    'Authorization' => 'Basic '.\base64_encode(\sprintf('%s:%s', $this->getClientId(), $this->getClientSecret())),
                 ],
             ]
         );
-        return $this->normalizeAccessTokenResponse((string)$response->getBody());
+
+        return $this->normalizeAccessTokenResponse((string) $response->getBody());
     }
 
     /**
-     * @param string $token
-     * @return array
      * @throws \GuzzleHttp\Exception\GuzzleException
+     *
      * @see https://developer.paypal.com/docs/api/identity/v1/#userinfo_get
      */
     protected function getUserByToken(string $token): array
@@ -148,22 +153,24 @@ class PayPal extends Base
             $this->userinfoURL,
             [
                 'headers' => [
-                    'Content-Type'  => 'application/x-www-form-urlencoded',
-                    'Authorization' => 'Bearer ' . $token,
+                    'Content-Type' => 'application/x-www-form-urlencoded',
+                    'Authorization' => 'Bearer '.$token,
                 ],
             ]
         );
+
         return $this->fromJsonBody($response);
     }
 
     #[Pure]
     protected function mapUserToObject(array $user): Contracts\UserInterface
     {
-        $user[Contracts\ABNF_ID]       = $user['user_id'] ?? null;
+        $user[Contracts\ABNF_ID] = $user['user_id'] ?? null;
         $user[Contracts\ABNF_NICKNAME] = $user['given_name'] ?? $user['family_name'] ?? $user['middle_name'] ?? null;
-        $user[Contracts\ABNF_NAME]     = $user['name'] ?? '';
-        $user[Contracts\ABNF_EMAIL]    = $user[Contracts\ABNF_EMAIL] ?? null;
-        $user[Contracts\ABNF_AVATAR]   = $user['picture'] ?? null;
+        $user[Contracts\ABNF_NAME] = $user['name'] ?? '';
+        $user[Contracts\ABNF_EMAIL] = $user[Contracts\ABNF_EMAIL] ?? null;
+        $user[Contracts\ABNF_AVATAR] = $user['picture'] ?? null;
+
         return new User($user);
     }
 }
